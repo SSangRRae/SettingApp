752f91e3a57f585765f60f334de9ab5213e5dbec
SSangRRae <pepe0003@kakao.com>
2023-11-17 20:10:11 +0900

    feat: UI sketch

--- SettingApp/ContentView.swift
+++ SettingApp/ContentView.swift
@@ -9,13 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Section {
+                    Text("SSang RRae")
+                }
+                Section {
+                    HStack {
+                        Image(systemName: "hourglass")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20)
+                            .padding(.all, 4)
+                            .background(.indigo)
+                            .foregroundColor(.white)
+                            .cornerRadius(5)
+                        NavigationLink("스크린 타임") {
+                            Text("스크린 타임 화면")
+                        }
+                    }
+                }
+                Section {
+                    HStack {
+                        Image(systemName: "gear")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20)
+                            .padding(.all, 4)
+                            .background(.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(5)
+                        NavigationLink("일반") {
+                            Text("일반 화면")
+                        }
+                    }
+                    HStack {
+                        Image(systemName: "accessibility")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20)
+                            .padding(.all, 4)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(5)
+                        NavigationLink("손쉬운 사용") {
+                            Text("손쉬운 사용 화면")
+                        }
+                    }
+                    HStack {
+                        Image(systemName: "hand.raised.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20, height: 20)
+                            .padding(.all, 4)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(5)
+                        NavigationLink("개인 정보 보호") {
+                            Text("개인 정보 보호 화면")
+                        }
+                    }
+                }
+            }
+            .navigationTitle(Text("설정"))
         }
-        .padding()
     }
 }
 
