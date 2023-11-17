b83bc3bf1ab49dd6553c779e0ed3f7bec13c9554
SSangRRae <pepe0003@kakao.com>
2023-11-17 17:37:47 +0900

    Initial Commit

---
+++ SettingApp/SettingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingAppApp.swift
+//  SettingApp
+//
+//  Created by SangRae Kim on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct SettingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
